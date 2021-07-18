3662ec4b467f166e83237c83fcf7339af6d61037
zakuro <z@kuro.red>
2021-07-18 12:37:02 +0900

    init

--- index.spec.ts
+++ index.spec.ts
@@ -1,3 +1,35 @@
-test('test', () => {
-  expect(1 + 1).toBe(2)
+import { unified } from 'unified'
+import parse from 'remark-parse'
+import stringify from 'remark-stringify'
+import extractToc from '.'
+
+const title = (...idxes: Array<number|null>) =>
+  idxes.map((v) => (v || '_')).join('-')
+
+test('title', () => {
+  expect(title(1, 1, 1)).toBe('1-1-1')
+  expect(title(1, null, 1)).toBe('1-_-1')
+})
+
+const source = `
+# ${title(1)}
+
+## ${title(1, 1)}
+
+### ${title(1, 1, 1)}
+
+# ${title(2)}
+
+### ${title(2, null, 1)}
+
+#### ${title(2, null, 1, 1)}
+`
+
+test('plain md', () => {
+  const md = unified()
+    .use(parse, {})
+    .use(extractToc)
+    .use(stringify)
+
+  md.process(source)
 })
--- index.ts
+++ index.ts
@@ -1,1 +1,9 @@
-export default function remarkExtractToc() {}
+import { Node } from 'unist'
+import { toc } from 'mdast-util-toc'
+
+export default function remarkStoreMetadataToc() {
+  return (node: Node) => {
+    const tocNode = toc(node)
+    console.log(JSON.stringify(tocNode, null, 2))
+  }
+}
